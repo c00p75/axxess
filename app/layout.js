491b62e087d30bd9ba068856492e284d88993e76
c00p75 <georgecoopmsapenda@gmail.com>
2025-06-09 14:11:31 +0200

    Add google tag

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import RootLayoutClient from "./RootLayoutClient";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import LoadingSrceen from "@/components/loading screen";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
   title: "Axxess Corporate",
@@ -13,11 +14,12 @@ level Consultants, covering all areas of service provision`,
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-3S6ETKZMJ0" />
       <body className="antialiased">
-        <LoadingSrceen /> 
-        <Navbar />  
+        <LoadingSrceen />
+        <Navbar />
         <RootLayoutClient>{children}</RootLayoutClient>
-        <Footer />             
+        <Footer />
       </body>
     </html>
   );
